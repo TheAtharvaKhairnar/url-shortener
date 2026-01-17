bc48064de87eba4b073d77ab1df853853c4b80c8
Atharva Khairnar <atharvakhairnar1505@gmail.com>
2026-01-17 17:59:33 +0530

    Configure for production deployement

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const {Pool}=require("pg");
-const pool=new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'URLShortener',
-    password: 'root',
-    port: 5432,
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:m3UcC0CPjbfdM1Tfyk3jjmB5Z8gnue8L@localhost:5432/URLShortener',
+    ssl: process.env.NODE_ENV==='production'?{rejectUnauthorized:false}:false
 });
-
 module.exports = pool;
 
